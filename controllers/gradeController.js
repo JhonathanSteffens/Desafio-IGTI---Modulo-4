39687004c2ae23c80ab1a3b5382cda6c9e179f54
jhonathan.steffens <jhony_steffens@hotmail.com>
2020-09-03 18:24:13 -0300

    "Arquivo alterado"

--- controllers/gradeController.js
+++ controllers/gradeController.js
@@ -1,10 +1,14 @@
 import { db } from '../models/index.js';
 import { logger } from '../config/logger.js';
+import Grades from '../models/Grades.js';
 
 const create = async (req, res) => {
   try {
+    let grade = req.body;
+    grade = new Grades(grade);
+    await grade.save();
     res.send({ message: 'Grade inserido com sucesso' });
-    logger.info(`POST /grade - ${JSON.stringify()}`);
+    logger.info(`POST /grade - ${JSON.stringify(req.body)}`);
   } catch (error) {
     res
       .status(500)
@@ -22,6 +26,8 @@ const findAll = async (req, res) => {
     : {};
 
   try {
+    const grades = await Grades.find(condition);
+    res.send(grades);
     logger.info(`GET /grade`);
   } catch (error) {
     res
@@ -35,6 +41,8 @@ const findOne = async (req, res) => {
   const id = req.params.id;
 
   try {
+    const grade = await Grades.findOne({ _id: id });
+    res.send(grade);
     logger.info(`GET /grade - ${id}`);
   } catch (error) {
     res.status(500).send({ message: 'Erro ao buscar o Grade id: ' + id });
@@ -50,9 +58,18 @@ const update = async (req, res) => {
   }
 
   const id = req.params.id;
+  let updateGrade = req.body;
+  delete updateGrade.lastModified;
+  delete updateGrade._id;
 
   try {
-    logger.info(`PUT /grade - ${id} - ${JSON.stringify(req.body)}`);
+    await Grades.replaceOne({ _id: id }, updateGrade, {
+      $set: { lastModified: '$$NOW' },
+    });
+    //updateGrade = await Grades.findOneAndUpdate({ _id: id }, updateGrade, { $set: { lastModified: "$$NOW" }});
+    updateGrade = await Grades.findOne({ _id: id });
+    logger.info(`PUT /grade - ${id} - ${updateGrade}`);
+    res.send(`PUT /grade - ${id} - ${updateGrade}`);
   } catch (error) {
     res.status(500).send({ message: 'Erro ao atualizar a Grade id: ' + id });
     logger.error(`PUT /grade - ${JSON.stringify(error.message)}`);
@@ -61,9 +78,14 @@ const update = async (req, res) => {
 
 const remove = async (req, res) => {
   const id = req.params.id;
-
   try {
-    logger.info(`DELETE /grade - ${id}`);
+    let grade = await Grades.findOne({ _id: id });
+    if (grade) {
+      grade = new Grades(grade);
+      await grade.deleteOne();
+      logger.info(`DELETE /grade - ${id}`);
+    }
+    res.send(`DELETE /grade - ${id}`);
   } catch (error) {
     res
       .status(500)
@@ -74,6 +96,8 @@ const remove = async (req, res) => {
 
 const removeAll = async (req, res) => {
   try {
+    const grades = await Grades.deleteMany({});
+    res.send(grades);
     logger.info(`DELETE /grade`);
   } catch (error) {
     res.status(500).send({ message: 'Erro ao excluir todos as Grades' });
